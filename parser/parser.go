af514b1584bddbfabba9eeec3fe65ab55c8130c5
Giacomo Marinangeli <jibbolo@gmail.com>
2016-11-13 13:35:40 +0100

    Add comments and unexport a constant

--- parser/parser.go
+++ parser/parser.go
@@ -7,10 +7,11 @@ import (
 	"time"
 )
 
-const MaxLocationNumber = 15
+const maxLocationNumber = 15
 
 var locationRgx = regexp.MustCompile(`([a-zA-Z]+)(:|\*|\_)(\+|\-)([0-9]{3})`)
 
+// Location represents node location
 type Location struct {
 	Name          string `json:"name"`
 	Status        string `json:"status"`
@@ -19,17 +20,20 @@ type Location struct {
 	Value         string `json:"value"`
 }
 
+// Result represents a single log record from /tmp/sql_state
 type Result struct {
 	Time      time.Time  `json:"time"`
 	Locations []Location `json:"locations"`
 	Raw       string     `json:"raw"`
 }
 
+// Parse takes raw bytes from /tmp/sql_state and convert them in Results
 func Parse(input []byte) (*Result, error) {
 	sec, err := strconv.Atoi(string(input[0:10]))
 	if err != nil {
 		return nil, fmt.Errorf("can't find time `%s: %s", input, err)
 	}
+
 	timestamp := time.Unix(int64(sec), 0)
 	res := &Result{
 		Time: timestamp,
@@ -39,7 +43,7 @@ func Parse(input []byte) (*Result, error) {
 	c := 0
 	for _, loc := range matches {
 		// Max 15 location!
-		if c == MaxLocationNumber {
+		if c == maxLocationNumber {
 			break
 		}
 		res.Locations = append(res.Locations, Location{
@@ -49,7 +53,7 @@ func Parse(input []byte) (*Result, error) {
 			PositiveValue: string(loc[3][0]),
 			Value:         string(loc[4]),
 		})
-		c += 1
+		c++
 	}
 	return res, nil
 }
